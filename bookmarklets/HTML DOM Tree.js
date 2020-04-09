87d8e379adf0a49f5c1088ff0d4632f156ae61d1
Sim <sim.neodin@gmail.com>
2020-04-09 07:48:32 +0200

    Update HTML DOM Tree.js

--- bookmarklets/HTML DOM Tree.js
+++ bookmarklets/HTML DOM Tree.js
@@ -1,1 +1,26 @@
-{\rtf1}
+function getChildren(obj, lvl) {
+  var ind = '';
+  for (var j = 0; j < lvl; j++) ind += '  ';
+  var s = '';
+  for (var i = 0; i < obj.childNodes.length; i++) {
+    var subobj = obj.childNodes[i];
+    if (subobj.nodeName.indexOf('#') == 0) s += '\n  ' + ind + subobj.nodeName;
+    else if (subobj.nodeName.indexOf('!') == 0) {
+      s += '\n  ' + ind + '</!-- (comment) -->';
+    } else {
+      s += '\n  ' + ind + '<' + subobj.nodeName + '>';
+      if (subobj.name) s += ' (' + subobj.name + ')';
+      s += getChildren(subobj, lvl + 1);
+      s += '\n  ' + ind + '</' + subobj.nodeName + '>';
+    }
+  }
+  return s;
+}
+var msg = '<HTML>' + getChildren(document.documentElement, 0) + '\n</HTML>';
+W = open('', '', 'resizable,scrollbars=1');
+with(W.document) {
+  writeln('<h3>' + document.location.href + '</h3>');
+  writeln('<xmp>' + msg + '</xmp>');
+  close();
+}
+void(0);
